41587510bd92a62127e3617f2341631ae9e7cd62
Ademar Latorre Junior <latorrejr@hotmail.com>
2025-03-23 14:30:44 -0300

    teste commit

---
+++ src/main/java/br/com/alatorre/controllers/PersonController.java
@@ -0,0 +1,35 @@
+package br.com.alatorre.controllers;
+
+import br.com.alatorre.PersonServices;
+import br.com.alatorre.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+//teste branch Secao6Aula47
+    @Autowired
+    private PersonServices service;
+
+    @RequestMapping(method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+    @RequestMapping(value = "/{Id}",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public Person findById(@PathVariable("Id") String id) {
+        return service.findById(id);
+    }
+    //comentário teste git commit
+}
